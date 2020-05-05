bd47dda37d6ac85aee3f9f188b4c2d22f959175b
Michael W <developerwakeling@gmail.com>
2020-05-04 23:14:58 -0400

    First Swift UI tutorial completed

---
+++ CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  SwiftUIIntroduction
+//
+//  Created by Michael Wakeling on 5/4/20.
+//  Copyright © 2020 Michael Wakeling. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+        
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
